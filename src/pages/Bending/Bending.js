f4204ab9337316c1ae6cf946f9c6b822dc7a1c72
jrfm <moesch.jonas@protonmail.com>
2023-04-16 15:36:23 +0200

    add basic bendingOutputs

--- src/pages/Bending/Bending.js
+++ src/pages/Bending/Bending.js
@@ -1,12 +1,14 @@
 import React from "react";
 import BendingInputs from "./BendingInputs";
+import BendingOutputs from "./BendingOutputs";
+import { MathJax } from "better-react-mathjax";
 
 function Bending() {
   return (
     <div id="bending--container">
       <h2>Einachsiger Biegespannungsnachweis:</h2>
       <BendingInputs />
-      {/* <BiegungOutput/> */}
+      <BendingOutputs />
     </div>
   );
 }
